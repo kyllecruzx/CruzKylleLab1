f174b9cbf41190d77af81a67a2bbd81dc7de13f8
cruzkyllecruz <cruzkyllecruz@gmail.com>
2018-09-05 20:22:04 +0800

    Initial commit

---
+++ app/src/main/java/com/cruz/kylle/Lab2.java
@@ -0,0 +1,13 @@
+package com.cruz.kylle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Lab2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.constraint);
+    }
+}
